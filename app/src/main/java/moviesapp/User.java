9177db1b9afa569764e856aa2cc0b6778694ed18
wassim-mouloud <simo.rko10@gmail.com>
2024-02-15 16:24:30 +0100

    adding user registration system

--- app/src/main/java/moviesapp/User.java
+++ app/src/main/java/moviesapp/User.java
@@ -1,30 +1,48 @@
 package moviesapp;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class User {
-    private int id;
     private String username;
+    private JSONArray favorites;
 
-    public User(int id, String username) {
-        this.id = id;
+    public User(String username) {
         this.username = username;
+        this.favorites = new JSONArray();
     }
 
-    // Getters et Setters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+    public User(JSONObject userJson) {
+        this.username = userJson.getString("username");
+        this.favorites = userJson.getJSONArray("favorites");
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public JSONArray getFavorites() {
+        return favorites;
     }
 
+    public JSONObject toJson() {
+        JSONObject userJson = new JSONObject();
+        userJson.put("username", username);
+        userJson.put("favorites", favorites);
+        return userJson;
+    }
 
+    public void addFavorite(JSONObject movie) {
+        favorites.put(movie);
+    }
+
+    public boolean isFavorite(JSONObject movie) {
+        for (int i = 0; i < favorites.length(); i++) {
+            JSONObject favorite = favorites.getJSONObject(i);
+            if (favorite.getString("title").equals(movie.getString("title"))) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
